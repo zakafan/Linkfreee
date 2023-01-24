012e5dda09ed998f273466a71434384c4a522f3d
zakafan <elyoussefizakaria52@gmail.com>
2023-01-24 16:24:24 +0100

    final

--- components/user/UserLink.js
+++ components/user/UserLink.js
@@ -15,31 +15,6 @@ export default function UserLink({
   const DisplayIcon = getIcon(link.icon);
 
   return (
-    <a
-      href={`${BASE_URL}/api/users/${username}/links/${encodeURIComponent(
-        link.url
-      )}`}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="rounded-full border-2 border-gray-200 hover:border-[color:var(--hover-color)] hover:shadow-xl p-4 my-2 w-full content-start flex flex-row gap-4 items-center"
-      style={{
-        "--hover-color": colors[link.icon],
-      }}
-      onClick={() => {
-        //Dans l'événement onClick, je vérifie si la variable clicked est false 
-        if (!clicked) {
-          //si c'est le cas, j'incrémente la variable clicks et je change la valeur de clicked à true.
-          setClicks(clicks + 1);
-          setClicked(true);
-          //la prochaine fois que l'utilisateur clique sur le même lien, il ne sera plus comptabilisé.
-        }
-      }}
-    >
-      <span style={{ color: colors[link.icon] }}>
-        <DisplayIcon />
-      </span>
-      <span className="grow">{link.name}</span>
-      {displayStatsPublic && <span>{clicks}</span>}
-    </a>
+    
   );
 }
